2d2a92153e821a9f8c12beb98dc615435a14a151
Shekhar Roy <43708251+Shekhar-roy-5292@users.noreply.github.com>
2024-09-23 02:01:03 +0530

    add search page

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,19 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
+import CategoryPage from "../pages/category/CategoryPage";
+import SearchPage from "../pages/search/SearchPage";
+import ShopPage from "../pages/shop/ShopPage";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/categories/:categoryName",
-        element: <div>Category page</div>,
-      },
+      { path: "/", element: <Home />},
+      { path: "/categories/:categoryName", element: <CategoryPage />},
+      { path: "/search", element: <SearchPage />},
+      { path: "/shop", element: <ShopPage />},
 
     ],
   },
